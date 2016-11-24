0ce65e8870ecb2b7e9e5bd88428822440246ee79
TheConnMan <bcconn2112@gmail.com>
2016-11-24 17:43:08 -0500

    Remove non-streaming code

--- streaming.js
+++ streaming.js
@@ -1,10 +1,9 @@
 var AWS = require('aws-sdk');
 var s3 = new AWS.S3();
+var phantomStream = require('phantom-render-stream')();
 var Twitter = require('twitter');
 var dateFormat = require('dateformat');
 var streamToBuffer = require('stream-to-buffer');
-
-var captureStreaming = require('./lib/screenshot').captureStreaming;
 
 var client = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -16,23 +15,28 @@ var client = new Twitter({
 var stream = client.stream('statuses/filter', {track: 'javascript'});
 stream.on('data', function(event) {
   if (event.text.indexOf('RT ') !== 0) {
-    streamToBuffer(captureStreaming(event.user.screen_name, event.id_str), function (err, buffer) {
-      try {
-        var params = {
-            Bucket: process.env.BUCKET,
-            Key: dateFormat(new Date(), 'yyyy/mm/dd/HH-MM-ss-') + event.id_str + '.png',
-            Body: new Buffer(buffer, 'binary')
-        };
-        s3.upload(params, function(err, result) {
-          if (err) {
-            console.log(err);
-          } else {
-            console.log('Successfully uploaded https://twitter.com/' + event.user.screen_name + '/status/' + event.id_str);
-          }
-        });
-      } catch (e) {
-        console.log(e);
-      }
-    });
+    processEvent(event.user.screen_name, event.id_str);
   }
 });
+
+function processEvent(user, statusId) {
+  var stream = phantomStream('https://twitter.com/' + user + '/status/' + statusId);
+  streamToBuffer(stream, function (err, buffer) {
+    try {
+      var params = {
+          Bucket: process.env.BUCKET,
+          Key: dateFormat(new Date(), 'yyyy/mm/dd/HH-MM-ss-') + statusId + '.png',
+          Body: new Buffer(buffer, 'binary')
+      };
+      s3.upload(params, function(err, result) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log('Successfully uploaded https://twitter.com/' + user + '/status/' + statusId);
+        }
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  });
+}
